267d26c19a3ef255436e79806b726fc67c98a2c8
Abel Cassinda <abelcassinda004@gmail.com>
2024-12-21 20:45:13 +0100

    instalando todos os componentes

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+// import Container from "@/components/modules/custom/Container";
+import React from "react";
+
+export default function NotFound() {
+  return (
+
+    // TODO:Custom not found page 
+    <section>
+      {/* <Container>Não encontrado </Container>; */}
+    <h1>Não encontrado</h1>
+    </section>
+  );
+}
